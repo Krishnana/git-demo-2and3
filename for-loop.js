582a81d872f343e1a221cfb3324e9e6ed0936eb8
krishnan <kaananth@amazon.com>
2019-02-15 12:00:54 -0800

    added logic

--- for-loop.js
+++ for-loop.js
@@ -1,7 +1,14 @@
 var stuff = ['pencils','costs','car','book','turtle'];
 var answer = prompt('what is one of my fav things?');
-var flag;
+var flag = 0;
 
 for(var i=0; i <stuff.length; i++){
-  
+  if(answer === stuff[i]){
+    alert('that\'s the correct thing');
+    flag = 1;
+  }
+}
+
+if(flag===0){
+  alert('you didn\'t find the right 1');
 }
